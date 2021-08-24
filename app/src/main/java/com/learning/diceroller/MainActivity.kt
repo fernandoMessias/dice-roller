7afb58f5ae1ed6ed14fd89def0e47e9246bd2f98
Fernando Da Silva <dasilvaf@amdocs.com>
2021-08-23 22:21:22 -0300

    Add toast message every time button is clicked

--- app/src/main/java/com/learning/diceroller/MainActivity.kt
+++ app/src/main/java/com/learning/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.learning.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
