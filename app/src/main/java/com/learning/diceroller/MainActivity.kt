1c891099e6e57906e24912c4c9a90cd970e2a964
Fernando Da Silva <dasilvaf@amdocs.com>
2021-08-23 22:29:02 -0300

    Implement the method to show a number between 1 and 6 when click in the button

--- app/src/main/java/com/learning/diceroller/MainActivity.kt
+++ app/src/main/java/com/learning/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.learning.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val ramdomInt = Random().nextInt(6) + 1
+
+        resultText.text = ramdomInt.toString()
     }
 }
