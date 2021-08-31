517af4445c5939846323ec5e5c1521c366dd89ce
Fernando Da Silva <dasilvaf@amdocs.com>
2021-08-30 22:53:39 -0300

    Small fixes in dice roller app

--- app/src/main/java/com/learning/diceroller/MainActivity.kt
+++ app/src/main/java/com/learning/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -31,7 +36,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
 
     }
