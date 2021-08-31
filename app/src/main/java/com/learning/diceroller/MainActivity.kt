601bb990688af2c7c19e85b7d5b7a366d9808613
Fernando Da Silva <dasilvaf@amdocs.com>
2021-08-30 22:16:23 -0300

    Add dice images and logic for dice rolling

--- app/src/main/java/com/learning/diceroller/MainActivity.kt
+++ app/src/main/java/com/learning/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.learning.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -20,10 +21,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
 
-        val ramdomInt = Random().nextInt(6) + 1
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
 
-        resultText.text = ramdomInt.toString()
     }
 }
